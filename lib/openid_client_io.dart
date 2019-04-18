9a769082a27f67ecd9397f2e7746e68f682367fe
Lukas Olsen <lukas@vertical-life.info>
2019-04-18 13:49:36 +0200

    changed structure and going for a return rather than a local server

--- lib/openid_client_io.dart
+++ lib/openid_client_io.dart
@@ -14,24 +14,21 @@ class Authenticator {
   final int port;
 
   Authenticator(Client client,
-      {this.port: 3000,
-      this.urlLancher: _runBrowser,
+      {this.urlLancher: _runBrowser,
       Iterable<String> scopes: const [],
       Uri redirectUri})
       : flow = redirectUri == null
             ? new Flow.authorizationCodeWithPKCE(client)
             : new Flow.authorizationCode(client)
           ..scopes.addAll(scopes)
-          ..redirectUri = redirectUri ?? Uri.parse("http://localhost:$port/cb");
+          ..redirectUri = redirectUri;
 
   Future<Credential> authorize() async {
     var state = flow.authenticationUri.queryParameters["state"];
 
     _requestsByState[state] = new Completer();
-    await _startServer(port);
-    urlLancher(flow.authenticationUri.toString());
 
-    var response = await _requestsByState[state].future;
+    var response = await urlLancher(flow.authenticationUri.toString());
 
     return flow.callback(response);
   }
@@ -39,50 +36,4 @@ class Authenticator {
   static Map<int, Future<HttpServer>> _requestServers = {};
   static Map<String, Completer<Map<String, String>>> _requestsByState = {};
 
-  static Future<HttpServer> _startServer(int port) async {
-    return _requestServers[port] ??=
-        (HttpServer.bind(InternetAddress.loopbackIPv4, port)
-          ..then((requestServer) async {
-            await for (var request in requestServer) {
-              request.response.statusCode = 200;
-              request.response.headers.set("Content-type", "text/html");
-              request.response.writeln("<html>"
-                  "<h1>You can now close this window</h1>"
-                  "<script>window.close();</script>"
-                  "</html>");
-              request.response.close();
-              var result = request.requestedUri.queryParameters;
-
-              if (!result.containsKey("state")) continue;
-              var r = _requestsByState.remove(result["state"]);
-              r.complete(result);
-              if (_requestsByState.isEmpty) {
-                for (var s in _requestServers.values) {
-                  (await s).close();
-                }
-                _requestServers.clear();
-              }
-            }
-
-            _requestServers.remove(port);
-          }));
-  }
-}
-
-void _runBrowser(String url) {
-  switch (Platform.operatingSystem) {
-    case "linux":
-      Process.run("x-www-browser", [url]);
-      break;
-    case "macos":
-      Process.run("open", [url]);
-      break;
-    case "windows":
-      Process.run("explorer", [url]);
-      break;
-    default:
-      throw new UnsupportedError(
-          "Unsupported platform: ${Platform.operatingSystem}");
-      break;
-  }
 }
